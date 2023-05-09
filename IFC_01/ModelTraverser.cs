d6c99a0120b6825332697b28a9c984184cc99b61
ge84his <ge84his@mytum.de>
2023-05-09 23:33:28 +0200

    First Commit

---
+++ IFC_01/ModelTraverser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using GeometryGym.Ifc;
+
+namespace IFC_01
+{
+    public class ModelTraverser
+    {
+        private DatabaseIfc _db;
+
+        public ModelTraverser(string ifcFilePath)
+        {
+            _db = new DatabaseIfc(ifcFilePath);
+        }
+
+        public void TraverseModel(string targetWallGlobalId)
+        {
+            // Select the wall using its GlobalId
+            var wall = _db.FirstOrDefault(x => x is IfcWall && (x as IfcWall).GlobalId.ToString() == targetWallGlobalId) as IfcWall;
+            Console.WriteLine($"Selected Wall - Name: {wall.Name}, GlobalId: {wall.GlobalId}");
+
+            // Find all instances of IfcOpeningElement related to the selected wall
+            var openingElements = _db.Where<IfcRelVoidsElement>(x => x.RelatingBuildingElement == wall).Select(x => x.RelatedOpeningElement);
+
+            Console.WriteLine($"Found {openingElements.Count()} opening elements for the selected wall");
+
+            // Iterate through the opening elements
+            foreach (var openingElement in openingElements)
+            {
+                Console.WriteLine($"\nOpening Element - GlobalId: {openingElement.GlobalId}");
+
+                // Find the corresponding IfcDoor or IfcWindow that fills the opening element
+                var fillingElement = _db.FirstOrDefault<IfcRelFillsElement>(x => x.RelatingOpeningElement == openingElement)?.RelatedBuildingElement;
+
+                if (fillingElement != null)
+                {
+                    Console.WriteLine($"Filling Element - Type: {fillingElement.GetType().Name}, GlobalId: {fillingElement.GlobalId}");
+                }
+                else
+                {
+                    Console.WriteLine("No filling element found for this opening element");
+                }
+            }
+        }
+    }
+}
---
+++ IFC_01/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryGym.Ifc;
+using System.Linq;
+
+namespace IFC_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the IFC file into the DatabaseIfc object
+            DatabaseIfc db = new DatabaseIfc(@"C:\Users\ataca\OneDrive\Masaüstü\23S\SemanticModeling\01_Exercise\Spaces-withBld.ifc");
+
+            // Create a list containing all entities in the IFC file
+            var allEntities = db.ToList();
+
+            // Use LINQ to select all IfcWall instances 
+            var allWalls = from wall in db.OfType<IfcWall>() select wall;
+
+
+            // Use lambda expressions to create a list containing all IfcWall instances
+            var allWallsLambda = db.OfType<IfcWall>().ToList();
+
+
+            // Iterate through all IfcWall instances selected with LINQ and print their GlobalId and Name
+            foreach (var wall in allWalls)
+            {
+                string GlobalId = wall.GlobalId;
+                string WallName = wall.Name;
+                Console.WriteLine(GlobalId);
+                Console.WriteLine(WallName);
+            }
+
+            // Use LINQ to select all IfcDoor instances
+            var allDoors = from door in db.OfType<IfcDoor>() select door;
+
+            // Use lambda expressions to create a list containing all IfcDoor instances
+            var allDoorsLambda = db.OfType<IfcDoor>().ToList();
+
+            // Iterate through all IfcDoor instances selected with LINQ and print their GlobalId and Name
+            foreach (var door in allDoors)
+            {
+                string GlobalId = door.GlobalId;
+                string DoorName = door.Name;
+                Console.WriteLine(GlobalId);
+                Console.WriteLine(DoorName);
+            }
+
+            // Iterate through all IfcDoor instances in the list created with lambda expressions and print their GlobalId and Name
+            foreach (var door in allDoorsLambda)
+            {
+                string GlobalId = door.GlobalId;
+                string DoorName = door.Name;
+                Console.WriteLine(GlobalId);
+                Console.WriteLine(DoorName);
+            }
+
+            // Comparing size of Walls and Doors 
+            Console.WriteLine($"Total number of the wall entites: {allWalls.Count()} \nTotal number of the door entites: {allDoors.Count()}");
+            //As we can see above there are 11 walls and 6 doors in the data.
+            //It is expected results actually while mostly we have one or two doors for each room, there are usually four walls.
+
+
+            // I found a wall that has two doors, its GlobalId: 3OIoKUJGfB9wDGaccZpWNf
+            string targetWallGlobalId = ("3OIoKUJGfB9wDGaccZpWNf");
+
+            // Define the path to your IFC file
+            string ifcFilePath = @"C:\Users\ataca\OneDrive\Masaüstü\23S\SemanticModeling\01_Exercise\Spaces-withBld.ifc";
+
+            // Create an instance of the ModelTraverser class and call the TraverseModel method
+            ModelTraverser modelTraverser = new ModelTraverser(ifcFilePath);
+            modelTraverser.TraverseModel(targetWallGlobalId);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
